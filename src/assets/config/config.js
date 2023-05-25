7fe25e5eafcfa4cc32668ac28800f2d5464aba58
Ben-Lazrek-Yassine <dogdevil181@gmail.com>
2023-05-25 11:57:13 +0100

    Update config.js

--- src/assets/config/config.js
+++ src/assets/config/config.js
@@ -5,12 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuPNWUVlIAjFKxteuu022LAa2mjKO7x0A",
-    authDomain: "react-6b32c.firebaseapp.com",
-    projectId: "react-6b32c",
-    storageBucket: "react-6b32c.appspot.com",
-    messagingSenderId: "1084217702516",
-    appId: "1:1084217702516:web:482973efb8841f8f3ff921"
+** :) **
   };
 
 export const logout = () => {
